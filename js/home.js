c08f3864f88d96f644c79d9b5678bb86e1b7e9bc
ziadayman6 <zayman655@gmail.com>
2024-06-14 17:44:30 +0300

    updating portfolio

--- js/home.js
+++ js/home.js
@@ -6,27 +6,7 @@ var container1 = document.querySelector('.container1');
 var container2 = document.querySelector('.container2');
 var container3 = document.querySelector('.container3');
 var container4 = document.querySelector('.container4');
-var hlines = document.querySelectorAll('.hline1, .hline2, .hline3, .hline4, .hline5');
-var numms = document.querySelectorAll('.numm');
-var nums = document.querySelectorAll('.num')
 var uparrow = document.getElementById('upp')
-
-const observer2 = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0 && !entry.target.classList.contains("counted")) {
-            counting(entry.target);
-            entry.target.classList.add("counted")
-        }
-    });
-}, { threshold: 0.1 });
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-            entry.target.classList.add('animate');
-        } 
-    });
-});
 
 arrow1.addEventListener('click', function() {
     if (container1.style.height === '0px' || !container1.style.height) {
@@ -45,35 +25,10 @@ arrow2.addEventListener('click', function() {
         container2.style.transition = 'height 1s ease'
         container2.style.height = container2.scrollHeight + 'px';
         arrow2.classList.add('rotated')
-            
-        hlines.forEach(hline => {
-            observer.observe(hline);
-        });            
-
-        numms.forEach(numm => {
-            numm.style.display = "block"
-        });            
-
-        nums.forEach(num => {
-            observer2.observe(num)
-        })
-
     } else {
         container2.style.height = '0px';
         container2.style.transition = 'height 0.5s ease'
         arrow2.classList.remove('rotated')
-
-        hlines.forEach(hline => {
-            hline.classList.remove('animate')
-        })
-
-        numms.forEach(numm => {
-            numm.style.display = "none"
-        });            
-
-        nums.forEach(num => {
-            num.classList.remove("counted")
-        })
     }
 });
 
@@ -100,15 +55,3 @@ arrow4.addEventListener('click', function() {
         arrow4.classList.remove('rotated')
     }
 });
-
-function counting(element){
-    var start = 0
-    var end = parseInt(element.getAttribute("data-val"))
-    var counter = setInterval(function(){
-        start += 1;
-        element.textContent = start
-        if (start == end) {
-            clearInterval(counter)
-        }
-    },3)
-}
